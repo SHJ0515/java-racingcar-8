31acc9a3b95bd8b4fc0815e5145bb9e00f35974d
송현준 <70439776+SHJ0515@users.noreply.github.com>
2025-10-27 15:42:57 +0900

    feat: 레이싱 결과 및 에러 출력용 OutputView 구현
    - 레이싱 중간 결과 출력 기능
    - 레이싱 우승자 출력 기능
    - 에러 출력 기능

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,4 +1,25 @@
 package racingcar.view;
 
+import racingcar.domain.Car;
+
+import java.util.List;
+
 public class OutputView {
+
+    public void printRoundResult(List<Car> cars) {
+        for (Car car : cars) {
+            String hyphens = "-".repeat(car.getCurrentLocation());
+            System.out.println(car.getName() + " : " + hyphens);
+        }
+        System.out.println();
+    }
+
+    public void printWinner(List<String> winnerList) {
+        String winners = String.join(",", winnerList);
+        System.out.println("최종 우승자 : " + winners);
+    }
+
+    public void printError(String errorMessage) {
+        System.out.println(errorMessage);
+    }
 }
