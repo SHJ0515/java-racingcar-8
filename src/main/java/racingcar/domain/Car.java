166f4499b5e87f739c8998cbaa3cc7be4a352f61
송현준 <70439776+SHJ0515@users.noreply.github.com>
2025-10-27 16:00:41 +0900

    feat: 사용자 입력에 맞는 개수의 자동차 생성 기능 구현

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,4 +1,19 @@
 package racingcar.domain;
 
 public class Car {
+    String name;
+    int currentLocation;
+
+    public Car(String name) {
+        this.name = name;
+        this.currentLocation = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCurrentLocation() {
+        return currentLocation;
+    }
 }
--- src/main/java/racingcar/service/RacingService.java
+++ src/main/java/racingcar/service/RacingService.java
@@ -1,4 +1,34 @@
 package racingcar.service;
 
+import racingcar.domain.Car;
+import racingcar.dto.RacingInfoDto;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class RacingService {
+
+    private List<Car> cars;
+    private int totalRound;
+    private int currentRound;
+
+    public void initRacing(RacingInfoDto racingInfoDto) {
+        this.cars = parseCar(racingInfoDto.getCars());
+        this.totalRound = Integer.parseInt(racingInfoDto.getRound());
+        this.currentRound = 0;
+    }
+
+    private List<Car> parseCar(String carNames) {
+
+        String[] names = carNames.split(",");
+        List<Car> carList = new ArrayList<>();
+
+        for (String name : names) {
+            carList.add(new Car(name));
+        }
+
+        return carList;
+    }
+
+
 }
