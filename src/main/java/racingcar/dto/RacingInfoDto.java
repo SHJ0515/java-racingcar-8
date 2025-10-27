f2518388c95da263261baaed8a1cca1712a8ae83
송현준 <70439776+SHJ0515@users.noreply.github.com>
2025-10-27 14:58:38 +0900

    feat: 사용자 입력용 InputView 구현
    - 자동차 이름 입력 기능
    - 시도할 횟수(라운드) 입력 기능
    - 입력값 RacingInfoDto로 반환

--- src/main/java/racingcar/dto/RacingInfoDto.java
+++ src/main/java/racingcar/dto/RacingInfoDto.java
@@ -1,4 +1,23 @@
 package racingcar.dto;
 
 public class RacingInfoDto {
+
+    private String cars;  // 자동차 이름 입력값
+    private String round;  //시행 횟수
+
+    public String getCars() {
+        return cars;
+    }
+
+    public void setCars(String cars) {
+        this.cars = cars;
+    }
+
+    public String getRound() {
+        return round;
+    }
+
+    public void setRound(String round) {
+        this.round = round;
+    }
 }
--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,4 +1,21 @@
 package racingcar.view;
 
+import camp.nextstep.edu.missionutils.Console;
+import racingcar.dto.RacingInfoDto;
+
 public class InputView {
+    private static final String INPUT_CAR_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String INPUT_ROUND = "시도할 횟수는 몇 회인가요?";
+
+    public RacingInfoDto input(){
+        RacingInfoDto racingInfoDto = new RacingInfoDto();
+
+        System.out.println(INPUT_CAR_NAME);
+        racingInfoDto.setCars(Console.readLine());
+
+        System.out.println(INPUT_ROUND);
+        racingInfoDto.setRound(Console.readLine());
+
+        return racingInfoDto;
+    }
 }
